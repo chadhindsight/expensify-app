2629050d27a91cb669ce5c64e558cdbf834ded92
Chad Hinds <chadhindsight@Chads-MacBook-Air.local>
2019-02-23 21:31:44 -0500

    Add number test

--- src/tests/add.test.js
+++ src/tests/add.test.js
@@ -2,4 +2,9 @@ const add = (a, b) => a + b;
 
 test('should add two numbers', () =>{
     const result = add(3, 4);
+    expect(result).toBe(7);
 });
+
+if(result !== 7) {
+ return new Error('Added wrong numbers ${result}');
+}
