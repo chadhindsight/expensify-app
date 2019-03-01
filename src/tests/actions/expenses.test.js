2bb958ce291b3988e12835884098bcfa9ba68f83
Chad Hinds <chadhindsight@Chads-MacBook-Air.local>
2019-02-28 21:39:36 -0500

    Add comment to expenses test

--- src/tests/actions/expenses.test.js
+++ src/tests/actions/expenses.test.js
@@ -1,6 +1,6 @@
 import {addExpense, editExpense, removeExpense} from '../../actions/expenses';
 
-// call our functions and assert something about the return value
+// Call our functions and assert something about the return value
 test('should setup remove expense action', ()=> {
     const action = removeExpense({ id: '123abc'});
     expect(action).toEqual({
