310614b03414729f940f2e887d4c8c45304634fd
Chad Hinds <chadhindsight@Chads-MacBook-Air.local>
2019-03-09 21:16:26 -0500

    test expenses reducer

--- src/tests/reducersTest/expenses.test.js
+++ src/tests/reducersTest/expenses.test.js
@@ -1,6 +1,7 @@
 import expensesReducer from '../../reducers/expensesReducer';
+import expenses from '../fixtures/expenses'
 
 test('should set default state', ()=>{
     const state = expensesReducer(undefined, {type: '@@INIT'});
     expect(state).toEqual([]);
-})
+});
