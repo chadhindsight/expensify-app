75b0b8db84e3739e5354baee71de5e1f996fb2d8
Chad Hinds <chadhindsight@Chads-MacBook-Air.local>
2019-03-13 19:02:14 -0400

    Setup config folders and files

--- src/tests/components/Header.test.js
+++ src/tests/components/Header.test.js
@@ -1,10 +1,11 @@
 // react-test-renderer for components
 import React from 'react';
-import ReactShallowRenderer from 'react-test-renderer/shallow';
+import {shallow} from 'enzyme';
 import Header from '../../components/Header';
 
 test('should render Header correctly', ()=>{
-    const renderer = new ReactShallowRenderer();
-    renderer.render(<Header />);
-    expect(renderer.getRenderOutput()).toMatchSnapshot();
+    const wrapper = shallow(<Header />)
+    expect(wrapper.find('h1').length).toBe(1);
+
+    
 });
