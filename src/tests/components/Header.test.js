a540b8403056eadfacb0e3dd8e00f1f98ce92d45
Chad Hinds <chadhindsight@Chads-MacBook-Air.local>
2019-03-12 22:12:41 -0400

    Update test render

---
+++ src/tests/components/Header.test.js
@@ -0,0 +1,10 @@
+// react-test-renderer for components
+import React from 'react';
+import ReactShallowRenderer from 'react-test-renderer/shallow';
+import Header from '../../components/Header';
+
+test('should render Header correctly', ()=>{
+    const renderer = new ReactShallowRenderer();
+    renderer.render(<Header />);
+    expect(renderer.getRenderOutput()).toMatchSnapshot();
+});
