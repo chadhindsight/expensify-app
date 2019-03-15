50bfc171ee3b908260e46a91924a12836d1dc82c
Chad Hinds <chadhindsight@Chads-MacBook-Air.local>
2019-03-15 15:31:53 -0400

    comment about header

--- src/tests/components/Header.test.js
+++ src/tests/components/Header.test.js
@@ -2,7 +2,7 @@
 import React from 'react';
 import { shallow } from 'enzyme';
 import Header from '../../components/Header';
-
+// cheese
 test('should render Header correctly', ()=>{
     const wrapper = shallow(<Header />)
     expect(wrapper.find('h1').length).toBe(1);
