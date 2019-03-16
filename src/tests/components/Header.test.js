2c53bb737c1f062db32e5802e67d55b20078175d
Chad Hinds <chadhindsight@Chads-MacBook-Air.local>
2019-03-16 17:20:16 -0400

    Setup Header test to work with enzyme by importing it

--- src/tests/components/Header.test.js
+++ src/tests/components/Header.test.js
@@ -5,7 +5,8 @@ import Header from '../../components/Header';
 // cheese
 test('should render Header correctly', ()=>{
     const wrapper = shallow(<Header />)
-    expect(wrapper.find('h1').length).toBe(1);
+    expect(wrapper).toMatchSnapshot();
+    // expect(wrapper.find('h1').text()).toBe('Expensify');
 
     
 });
