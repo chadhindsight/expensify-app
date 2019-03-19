3ea4d67ccb3ccbc278b829d974bf6b3817b44e00
Chad Hinds <chadhindsight@Chads-MacBook-Air.local>
2019-03-19 19:31:47 -0400

    setup mock data for dynamic component testing

--- src/tests/components/ExpenseForm.test.js
+++ src/tests/components/ExpenseForm.test.js
@@ -11,5 +11,61 @@ test('should render ExpenseForm correctly', () =>{
 test('should render form with data', ()=>{
     const wrapper = shallow(<ExpenseForm expense={expenses[2]}/>)
     expect(wrapper).toMatchSnapshot();
-
 })
+
+test('should render error for invalid form submission', () =>{
+    const wrapper = shallow(<ExpenseForm/>)
+    expect(wrapper).toMatchSnapshot();
+    wrapper.find('form').simulate('submit', {
+        preventDefault: () => {}
+    });
+    expect(wrapper.state('error').length).toBeGreaterThan(0);
+    expect(wrapper).toMatchSnapshot();
+});
+
+test('should set description on event change', ()=>{
+    const value = 'New description';
+    const wrapper = shallow(<ExpenseForm />);
+    // simulate change event
+    wrapper.find('input').at(0).simulate('change',{
+        target: {value}
+    });
+    expect(wrapper.state('description')).toBe(value);
+})
+
+test('should set note on textarea change', ()=>{
+    const value = 'New note';
+    const wrapper = shallow(<ExpenseForm />);
+    wrapper.find('textarea').simulate('change', {
+        target: { value }
+    });
+    expect(wrapper.state('note')).toBe(value);
+})
+
+test('should set amount if valid input', ()=>{
+    const value = '23.65';
+    const wrapper = shallow(<ExpenseForm />);
+    wrapper.find('input').at(1).simulate('change', {
+        target: { value }
+    });
+    expect(wrapper.state('amount')).toBe(value);
+})
+
+test('should not set amount if valid input', () => {
+    const value = '12.122';
+    const wrapper = shallow(<ExpenseForm />);
+    wrapper.find('input').at(1).simulate('change', {
+        target: { value }
+    });
+    expect(wrapper.state('amount')).toBe('');
+})
+
+test('should call onsubmit for valid prop form submission', ()=>{
+    const onSubmitSpy = jest.fn();
+    const wrapper = shallow(<ExpenseForm expense={expenses[0]} onSubmit={onSubmitSpy}/>)
+    wrapper.find('form').simulate('submit', {
+        preventDefault: () => { }
+    });
+    expect(wrapper.state('error')).toBe('');
+    expect(onSubmitSpy).toHaveBeenLastCalledWith();
+});
