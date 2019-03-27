4c3f4d9ebba844db03395d7bc5c573322126549c
Chad Hinds <chadhindsight@Chads-MacBook-Air.local>
2019-03-27 15:15:06 -0400

    Setup heroku

--- server/server.js
+++ server/server.js
@@ -9,6 +9,6 @@ app.use(express.static(publicPath));
 app.get('*', (req, res)=>{
     res.sendFile(path.join(publicPath, 'index.html'));
 });
-app.listen(3000, () =>{
+app.listen(port, () =>{
     console.log('Server is up!');
 });
