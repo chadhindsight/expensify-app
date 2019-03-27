998017784e0c0a8299ad8bed5640789e8875acee
Chad Hinds <chadhindsight@Chads-MacBook-Air.local>
2019-03-27 14:51:51 -0400

    Setup Production build and server with heroku

--- webpack.config.js
+++ webpack.config.js
@@ -18,14 +18,28 @@ module.exports = (env) => {
         exclude: /node_modules/
       }, {
         test: /\.s?css$/,
-        use: [
-          'style-loader',
-          'css-loader',
-          'sass-loader'
-        ]
+        use: CSSExtract.extract({
+          use: [
+            {
+              loader: 'css-loader',
+              options: {
+                sourceMap: true
+              }
+            },
+            {
+              loader: 'sass-loader',
+              options:{
+                sourceMap: true 
+              }
+            }  
+          ]
+        })
       }]
     },
-    devtool: isProduction? "source-map" :'cheap-module-eval-source-map',
+    plugins: [
+      CSSExtract
+    ],
+    devtool: isProduction? "source-map" :'inline-source-map',
     devServer: {
       contentBase: path.join(__dirname, 'public'),
       historyApiFallback: true
